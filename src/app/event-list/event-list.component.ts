9d165279136f9f56a5d30c6209fecaa314c12dd3
ednakaha <ednakaha@gmail.com>
2019-02-11 14:41:41 +0200

    ROUTE

--- src/app/event-list/event-list.component.ts
+++ src/app/event-list/event-list.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-event-list',
   templateUrl: './event-list.component.html',
   styleUrls: ['./event-list.component.css']
 })
--- src/app/manu-item/manu-item.component.ts
+++ src/app/manu-item/manu-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-manu-item',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManuItemComponent implements OnInit {
 
+  @Input() menuItem;
   constructor() { }
 
   ngOnInit() {
